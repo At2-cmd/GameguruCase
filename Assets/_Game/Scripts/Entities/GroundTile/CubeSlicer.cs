98022f13f58992104efaa71aec295635824901da
Ekrem Atakan Kuloğlu <atakankuloglu1997@gmail.com>
2025-03-01 14:57:08 +0300

    Seperate slicer class into methods for better readability

--- Assets/_Game/Scripts/Entities/GroundTile/CubeSlicer.cs
+++ Assets/_Game/Scripts/Entities/GroundTile/CubeSlicer.cs
@@ -16,31 +16,46 @@ public class CubeSlicer : MonoBehaviour
 
     void SliceCube()
     {
-        float frontXMin = frontCube.position.x - frontCube.localScale.x / 2;
-        float frontXMax = frontCube.position.x + frontCube.localScale.x / 2;
-        float backXMin = backCube.position.x - backCube.localScale.x / 2;
-        float backXMax = backCube.position.x + backCube.localScale.x / 2;
+        float frontXMin, frontXMax, backXMin, backXMax;
+        CalculateBounds(out frontXMin, out frontXMax, out backXMin, out backXMax);
 
         if (frontXMin < backXMin || frontXMax > backXMax)
         {
-            float overlapStart = Mathf.Max(frontXMin, backXMin);
-            float overlapEnd = Mathf.Min(frontXMax, backXMax);
-            float overlapSize = overlapEnd - overlapStart;
+            SliceFrontCube(frontXMin, frontXMax, backXMin, backXMax);
+        }
+    }
 
-            frontCube.position = new Vector3(overlapStart + overlapSize / 2, frontCube.position.y, frontCube.position.z);
-            frontCube.localScale = new Vector3(overlapSize, frontCube.localScale.y, frontCube.localScale.z);
+    void CalculateBounds(out float frontXMin, out float frontXMax, out float backXMin, out float backXMax)
+    {
+        frontXMin = frontCube.position.x - frontCube.localScale.x / 2;
+        frontXMax = frontCube.position.x + frontCube.localScale.x / 2;
+        backXMin = backCube.position.x - backCube.localScale.x / 2;
+        backXMax = backCube.position.x + backCube.localScale.x / 2;
+    }
 
-            // Create excess part with Rigidbody to drop
-            float excessSize = Mathf.Abs(frontXMax - backXMax) > 0.01f ? Mathf.Abs(frontXMax - backXMax) : Mathf.Abs(frontXMin - backXMin);
-            if (excessSize > 0.01f)
-            {
-                float excessX = (frontXMax > backXMax) ? frontXMax - excessSize / 2 : frontXMin + excessSize / 2;
-                GameObject excessCube = Instantiate(cubePrefab, new Vector3(excessX, frontCube.position.y, frontCube.position.z), Quaternion.identity);
-                excessCube.transform.localScale = new Vector3(excessSize, frontCube.localScale.y, frontCube.localScale.z);
+    void SliceFrontCube(float frontXMin, float frontXMax, float backXMin, float backXMax)
+    {
+        float overlapStart = Mathf.Max(frontXMin, backXMin);
+        float overlapEnd = Mathf.Min(frontXMax, backXMax);
+        float overlapSize = overlapEnd - overlapStart;
 
-                Rigidbody rb = excessCube.AddComponent<Rigidbody>();
-                rb.mass = 1f;
-            }
+        frontCube.position = new Vector3(overlapStart + overlapSize / 2, frontCube.position.y, frontCube.position.z);
+        frontCube.localScale = new Vector3(overlapSize, frontCube.localScale.y, frontCube.localScale.z);
+
+        CreateExcessPart(frontXMin, frontXMax, backXMin, backXMax, overlapSize);
+    }
+
+    void CreateExcessPart(float frontXMin, float frontXMax, float backXMin, float backXMax, float overlapSize)
+    {
+        float excessSize = Mathf.Abs(frontXMax - backXMax) > 0.01f ? Mathf.Abs(frontXMax - backXMax) : Mathf.Abs(frontXMin - backXMin);
+        if (excessSize > 0.01f)
+        {
+            float excessX = (frontXMax > backXMax) ? frontXMax - excessSize / 2 : frontXMin + excessSize / 2;
+            GameObject excessCube = Instantiate(cubePrefab, new Vector3(excessX, frontCube.position.y, frontCube.position.z), Quaternion.identity);
+            excessCube.transform.localScale = new Vector3(excessSize, frontCube.localScale.y, frontCube.localScale.z);
+
+            Rigidbody rb = excessCube.AddComponent<Rigidbody>();
+            rb.mass = 1f;
         }
     }
 }
